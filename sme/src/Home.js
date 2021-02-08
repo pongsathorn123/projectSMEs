280d80974c0d770a1524b8b08d86845e221d0431
Oui226 <60050255@kmitl.ac.th>
2021-02-08 23:24:48 +0700

    Edit home views

--- sme/src/Home.js
+++ sme/src/Home.js
@@ -4,7 +4,8 @@ import Nav_owner from './components/Nav_owner';
 import Nav_notlogin from './components/Nav_notlogin';
 import Nav_inves from './components/Nav_inves';
 import cookies from "./cookies/cookie";
-import { Container, Col, Row, Form } from 'react-bootstrap'
+import { Container, Col, Row, Form ,Button} from 'react-bootstrap'
+import sme1 from '../image/sme1.png';
 
 
 class createProject extends Component{
@@ -16,6 +17,17 @@ render(){
             {(cookies.get('type') == "owner") ? <Nav_owner /> : <Nav_inves />}
             <div>
                  <center> <h1>Home</h1> </center>
+                 <Card style={{ width: '18rem' ,marginLeft : 50 }}>
+                  <Card.Img variant="top" src="sme1/100px180" />
+                      <Card.Body>
+                          <Card.Title>Project 1</Card.Title>
+                          <Card.Text>
+                          The company deals with the export of food.
+                          Foreign country       
+                          </Card.Text>
+                        <Button variant="primary"> Lead more</Button>
+                    </Card.Body>
+                  </Card>
          </div>
         </div>
 
