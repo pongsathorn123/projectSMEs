075f96e7aa46e4819ac92c3acd12bb8688f18889
Oui226 <60050255@kmitl.ac.th>
2021-02-08 23:56:58 +0700

    modify home views

--- sme/src/Home.js
+++ sme/src/Home.js
@@ -4,8 +4,7 @@ import Nav_owner from './components/Nav_owner';
 import Nav_notlogin from './components/Nav_notlogin';
 import Nav_inves from './components/Nav_inves';
 import cookies from "./cookies/cookie";
-import { Container, Col, Row, Form ,Button} from 'react-bootstrap'
-import sme1 from '../image/sme1.png';
+import { Container, Col, Row, Form ,Button ,} from 'react-bootstrap';
 
 
 class createProject extends Component{
@@ -16,11 +15,47 @@ render(){
              
             {(cookies.get('type') == "owner") ? <Nav_owner /> : <Nav_inves />}
             <div>
-                 <center> <h1>Home</h1> </center>
-                 <Card style={{ width: '18rem' ,marginLeft : 50 }}>
+                 <center> <h1>SMES Project</h1> </center>
+                <h6 style= {{ marginLeft: 80 , marginRight:50}}>
+                Finding Project for investment and all project in FundingChain was proof by
+                  blockchain technology,It make your invest better and easier. Let's fund
+                  Finding Project for investment and all 
+                  project in FundingChain was proof by
+                  blockchain technology,It make your 
+                  invest better and easier. Let's fund
+                  Finding Project for investment and all project in FundingChain was proof by
+                  blockchain technology,It make your 
+                  invest better and easier. Let's fund
+                  Finding Project for investment and all project in FundingChain was proof by
+                  blockchain technology,It make your invest better and easier. Let's fund
+                  Finding Project for investment and all project in FundingChain was proof by
+                  blockchain technology,It make your invest better and easier. Let's fund
+                </h6>
+
+                <br/>
+                <br/>
+
+                <Container>
+                  <Row>
+                    <Col>
+                        <Card   border="primary" style={{ width: '18rem' ,marginLeft : 50 ,marginTop:10}}>
+                            <Card.Img variant="top" src="sme1/100px180" />
+                            <Card.Body>
+                            <Card.Title>Project 1</Card.Title>
+                            <Card.Text>
+                          The company deals with the export of food.
+                          Foreign country       
+                          </Card.Text>
+                        <Button variant="primary"> Lead more</Button>
+                        </Card.Body>
+                         </Card>
+                     </Col>
+
+                <Col xs={3}>
+                  <Card  border="primary" style={{ width: '18rem' ,marginLeft : 0, marginTop:10 }}>
                   <Card.Img variant="top" src="sme1/100px180" />
                       <Card.Body>
-                          <Card.Title>Project 1</Card.Title>
+                          <Card.Title>Project 2</Card.Title>
                           <Card.Text>
                           The company deals with the export of food.
                           Foreign country       
@@ -28,6 +63,23 @@ render(){
                         <Button variant="primary"> Lead more</Button>
                     </Card.Body>
                   </Card>
+                </Col>
+
+                  <Col>
+                  <Card  border="primary" style={{ width: '18rem' ,marginLeft : 100, marginTop:10 }}>
+                  <Card.Img variant="top" src="sme1/100px180" />
+                      <Card.Body>
+                          <Card.Title>Project 3</Card.Title>
+                          <Card.Text>
+                          The company deals with the export of food.
+                          Foreign country       
+                          </Card.Text>
+                        <Button variant="primary"> Lead more</Button>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              </Row>
+            </Container>
          </div>
         </div>
 
